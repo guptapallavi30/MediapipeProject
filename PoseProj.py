d4a452a40882e814a0e69e7e155876c4cdbef111
Pallavi  Gupta <pallavigupta@pari-mac.lan>
2022-03-21 17:43:29 -0700

    webcam

--- PoseProj.py
+++ PoseProj.py
@@ -2,15 +2,16 @@ import cv2
 import time
 import PoseModule as pm
 
-cap = cv2.VideoCapture('./PoseVideos/2.mp4')
+# cap = cv2.VideoCapture('./PoseVideos/2.mp4')
+cap = cv2.VideoCapture(0)
 pTime = 0
 detector = pm.poseDetector();
 while True:
     success, img = cap.read() # returns true if image is read (false ow), and the array of images (empty ow)
     img = detector.findPose(img)
     lmList = detector.findPosition(img)
-    print(lmList[14])
-    cv2.circle(img, (lmList[14][1], lmList[14][2]), 40, (255, 0, 255), cv2.FILLED) # draw over point with bigger circle
+    # print(lmList[14])
+    # cv2.circle(img, (lmList[14][1], lmList[14][2]), 40, (255, 0, 255), cv2.FILLED) # draw over point with bigger circle
 
     if not success:
         break
